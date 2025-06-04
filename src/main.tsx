f831fa35c2bea0bf417e291e95cd474c2549c74a
Grail-Lin <grail.lin@gmail.com>
2025-06-04 19:32:53 +0800

    先revert回可以執行的畫面

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './landing/App_landing.jsx';
+import { HashRouter } from 'react-router-dom';
+
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/baba_test">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/main" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter >
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
