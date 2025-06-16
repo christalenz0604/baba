7a87de0d4b194a6fa2942ffec23c456793ef0e4d
Christa <christalenz800@gmail.com>
2025-06-16 16:50:12 +0800

    洪孟楷題目文本&份數

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1022,122 +1022,142 @@ const YEHYUANCHIHQuestions: Question[] = [
 const HungMongKaiQuestions: Question[] = [
   {
     id: 'HungMongKai1',
-    text: 'WBC世界棒球經典賽資格賽昨晚（25日）落幕，美國在台協會昨天晚間在臉書發文『恭喜台灣打進2026世界棒球經典賽』;洪孟楷也寫下：「感謝AIT！希望2026WBC中華隊跟美國隊有機會碰頭』;身為洪委員的支持者,你的社群回應？',
+    text: '壯促法推動被北檢調查圖利疑雲，你怎麼說？',
     image: 'HungMongKai1.jpg',
     options: [
-      { id: 'HungMongKai1-1', text: '委員棒棒！！Go, Taiwan  go!!', points: 10, isCorrect: true },
-      { id: 'HungMongKai1-2', text: '體育噌美國 政治噌中國？', points: 5, isCorrect: false },
-      { id: 'HungMongKai1-3', text: '人家寫TAIWAN,你在那邊中華隊？？？', points: 3, isCorrect: false },
+      { id: 'HungMongKai1-1', text: '又來了啦，這是綠營最愛玩的「人格毀滅遊戲」', points: 16686, isCorrect: true },
+      { id: 'HungMongKai1-2', text: '先靜觀其變，反正不是第一個案子。', points: 5, isCorrect: false },
+      { id: 'HungMongKai1-3', text: '啊？吳春城那天講話我其實沒聽懂啦⋯⋯', points: 3, isCorrect: false },
     ],
+    explanation: {
+      text: '洪孟楷是『壯世代專法』的國民黨主連署和提案人。初審的時候（2024/10/24），蘇永富處長（行政院內政衛福勞動處）一直強調『壯世代有超過13%的失能者』，所以真的不能訂一個『壯世代專法』就把 55歲以上的人都劃進來。洪孟楷問說，『處長，你有沒有看過，有媒體報導上街頭遊行，51歲的老翁也站出來了！』洪孟楷覺得這樣的新聞報導『怪怪的』，他覺得如果體態保持良好、身體健康、肌力夠，連71歲的人都不一定可以稱老翁，所以他要行政院提『壯世代專法』的版本。」',
+      reference: 'https://news.ltn.com.tw/news/politics/breakingnews/4948304'
+    }
   },
   {
     id: 'HungMongKai2',
-    text: '今天洪孟楷終於現身林口,且國民黨黨主席朱立倫與地方議員陪同掃街,宣傳反罷免;平日不太在林口走動的他,如今動作頻頻,對此,你的回應？',
+    text: '上班時間總召邀你一起去中國查訪',
     image: 'HungMongKai2.jpg',
     options: [
-      { id: 'HungMongKai2-1', text: '都是賴清德害的', points: 12, isCorrect: true },
-      { id: 'HungMongKai2-2', text: '你只有選舉到了才會來林口！', points: 6, isCorrect: false },
-      { id: 'HungMongKai2-3', text: '不要說林口，連淡水也從來沒看過……', points: 4, isCorrect: false },
+      { id: 'HungMongKai2-1', text: '嗯？我睡著了，沒有注意到，他們出發了嗎？', points: 1450, isCorrect: true },
+      { id: 'HungMongKai2-2', text: '謝謝總召，但我還是想認真開會為民服務。', points: 6, isCorrect: false },
+      { id: 'HungMongKai2-3', text: '我們是在立法院上班，立院總召不是統戰團行程總召好嗎？', points: 4, isCorrect: false },
     ],
+    explanation: {
+      text: '2024/4/26在立法院開議期間，傅崐萁率領羅明才、陳雪生、鄭天財、徐欣瑩、陳玉珍、鄭正鈐、王鴻薇、林沛祥、廖先翔、張智倫、邱鎮軍、游顥、盧縣一、黃仁、翁曉玲、林倩綺訪問中國，在人民大會堂新疆廳拜見全國政協主席王滬寧；王滬寧近日才宣布去年統戰工作做得很好，2025更要做好「四個凝聚」工作，切實發揮最廣泛愛國統一戰線組織的政治作用，不斷鞏固全國各族人民大團結，加強海內外中華兒女大團結。',
+      reference: 'https://www.cna.com.tw/news/aipl/202404260199.aspx'
+    }
   },
   {
     id: 'HungMongKai3',
-    text: '缺特色題3',
+    text: '不知道為什麼，黨團突然說要提案中配取得身分證年限從六年改為四年。',
     options: [
-      { id: 'HungMongKai3-1', text: 'Supportive and harmonious', points: 10, isCorrect: true },
-      { id: 'HungMongKai3-2', text: 'Innovative and stimulating', points: 5, isCorrect: false },
-      { id: 'HungMongKai3-3', text: 'Structured and organized', points: 3, isCorrect: false },
+      { id: 'HungMongKai3-1', text: '本來就已經有特殊待遇，沒必要再開綠燈了。', points: 10, isCorrect: false },
+      { id: 'HungMongKai3-2', text: '我不能提這種幫中國洗人口的提案。', points: 5, isCorrect: false },
+      { id: 'HungMongKai3-3', text: '選票多才是真的好，以後感恩我們的中配會更多！', points: 6666, isCorrect: true },
     ],
+    explanation: {
+      text: '國民黨立委提案修法擬將中國籍配偶取得身分證時間從6年縮短成4年，。現有政策為中配依親滿4年、連續長居2年即可申請身分證，不像外籍配偶須先放棄原有國籍才能取得我國身分證。還能讓親人來台享用健保，國民黨為何此時要縮短時程、啟人疑竇呢？',
+      reference: 'https://news.pts.org.tw/article/733781'
+    }
   },
   {
     id: 'HungMongKai4',
-    text: '在準備強行通過《選罷法》、《憲訴法》與《財劃法》三讀前夕，民進黨立委說要摸黑闖入立法院，佔領主席台。你打算？',
+    text: '立法院部分黨團提出了65歲以上免健保費一案，在深入研究後，我決定',
     image: 'HungMongKai4.jpg',
     options: [
-      { id: 'HungMongKai4-1', text: '大聲放話：「你們全部進去啦，我就把它燒掉。」', points: 15, isCorrect: true },
-      { id: 'HungMongKai4-2', text: '給總召摸頭，一摸泯恩仇', points: 8, isCorrect: false },
-      { id: 'HungMongKai4-3', text: '和總召商量，號召大家一起去守夜', points: 5, isCorrect: false },
+      { id: 'HungMongKai4-1', text: '研究什麼啦，按鈕在哪我就按哪。', points: 4277, isCorrect: true },
+      { id: 'HungMongKai4-2', text: '反對，很多65歲以上是房東收租大戶，這提案會被罵爆。', points: 8, isCorrect: false },
+      { id: 'HungMongKai4-3', text: '反對，沒事找事造成世代對立做啥', points: 5, isCorrect: false },
     ],
+    explanation: {
+      text: '藍白針對《老人福利法》修正草案提案，原本最寬鬆版本為65歲以上長者，綜合所得稅率未達20%免繳健保費，沒想到引發醫事團體反對，醫改團體更警告，全民健保是「社會保險」，而非「社會福利」，修法若通過恐怕衝擊健保永續。',
+      reference: 'https://news.pts.org.tw/article/731315'
+    }
   },
   {
     id: 'HungMongKai5',
-    text: '關於藍白二黨所提出的《立法院職權行使法》條例，下列何者為貴黨的提案內容？',
+    text: '黨團要修《立法院職權行使法》條例，你建議如何做？',
     image: 'HungMongKai5.jpg',
     options: [
-      { id: 'HungMongKai5-1', text: '增訂「藐視國會罪」', points: 12, isCorrect: true },
-      { id: 'HungMongKai5-2', text: '立法院會議，須有立法委員總額三分之一出席，始得開會。', points: 6, isCorrect: false },
-      { id: 'HungMongKai5-3', text: '陽光來了，貪腐再見', points: 4, isCorrect: false },
+      { id: 'HungMongKai5-1', text: '該開公聽會就開，該審慎就審慎，不然又被說是在搞立法獨裁。', points: 12, isCorrect: false },
+      { id: 'HungMongKai5-2', text: '最後一秒上機密版本，一起舉手秒通過，爽。', points: 6287, isCorrect: true },
+      { id: 'HungMongKai5-3', text: '理性討論，找到對台灣最有利的平衡點。', points: 4, isCorrect: false },
     ],
+    explanation: {
+      text: '2024/5/21在場外大批民要求退回委員會重新審查《立法院職權行使法》狀況下，在最後一刻，藍白兩黨提出沒有被實質討論過的最高機密版本，並以舉手表決方式強行通過。被憲法法庭認定有諸多違憲的法案，該法案賦予立法院調查權，有權利要公私部門甚至個人提供這各種資訊，且設定禁止反質詢規定，問為什麼或拒絕提供就是處罰，在缺乏第三方制衡下，形同一種強力的空白授權。',
+      reference: 'https://watchout.tw/reports/KK9ll6K03prNMxllAru8'
+    }
   },
   {
     id: 'HungMongKai6',
-    text: '黨主席朱立倫邀請各位426上凱道「反綠共戰獨裁」，身為忠貞的黨團立委，你會？',
+    text: '你同事徐欣瑩用鎖住會議室大絕招，用三分鐘把超嚴格選罷法提案送出委員會，你怎麼看？',
     image: 'HungMongKai6.jpg',
     options: [
-      { id: 'HungMongKai6-1', text: '動員外籍移工一起上街', points: 10, isCorrect: true },
-      { id: 'HungMongKai6-2', text: '去問問盧秀燕那天有沒有空', points: 5, isCorrect: false },
-      { id: 'HungMongKai6-3', text: '打電話給蔣萬安', points: 3, isCorrect: false },
+      { id: 'HungMongKai6-1', text: '法案應該要公開討論，不是玩暗黑通關。', points: 10, isCorrect: false },
+      { id: 'HungMongKai6-2', text: '鎖門是必要手段，太多雜音就會拖進度。', points: 1949, isCorrect: true },
+      { id: 'HungMongKai6-3', text: '不行啦，這根本戒嚴2.0，連我媽都看不下去。', points: 3, isCorrect: false },
     ],
+    explanation: {
+      text: '公視報導，立法院內政委員會2024/12/16排審《選罷法》修正草案，國民黨拂曉出擊佔領立院紅樓2樓，將一側的鐵門鎖上用長桌頂住；並用保鮮膜把內政委員會門口封住；另一側則是以人牆阻擋，只讓議事人員進入。9時一到，召委徐欣瑩立即宣布開會，念完相關條文宣布初審通過，全數條文保留協商。',
+      reference: 'https://news.pts.org.tw/article/729015'
+    }
   },
   {
     id: 'HungMongKai7',
-    text: '上班時間總召邀你一起去中國查訪',
+    text: '跟主席朱立倫掃街反罷免，民眾會怎樣？',
     image: 'HungMongKai7.jpg',
     options: [
-      { id: 'HungMongKai7-1', text: '好，總召說甚麼是甚麼', points: 15, isCorrect: true },
-      { id: 'HungMongKai7-2', text: '不行，我要為民服務上班', points: 7, isCorrect: false },
-      { id: 'HungMongKai7-3', text: '是立院總召還是統戰團總召，我有沒聽錯，當然要拒絕。', points: 4, isCorrect: false },
+      { id: 'HungMongKai7-1', text: '有些會嗆，但還是有人搶著跟我自拍啦～', points: 15, isCorrect: false },
+      { id: 'HungMongKai7-2', text: '我耶！這區我得票最高欸，會熱烈歡迎啦！', points: 7, isCorrect: false },
+      { id: 'HungMongKai7-3', text: '嗚嗚，有人跑去跟主席抱怨我失聯已久⋯⋯', points: 10000, isCorrect: true },
     ],
+    explanation: {
+      text: '當朱立倫和洪孟楷沿著市場和攤販打招呼握手，來到其中一個攤位時，一名女子在朱立倫耳邊說「我告訴你吼！」朱立倫側身準備聆聽女子想說什麼，結果女子竟是來告狀，稱洪孟楷只有要選票時才會到我們林口，「然後從來就沒有來過」。',
+      reference: 'https://www.youtube.com/watch?v=8ZacalwrDKk'
+    }
   },
   {
     id: 'HungMongKai8',
-    text: '不知道為什麼，黨團突然說要提案中配取得身分證年限從六年改為四年。',
+    text: '明天要審《選罷法》、《憲訴法》與《財劃法》三讀前夕，民進黨立委說要摸黑闖入立法院，佔領主席台。你打算？',
     image: 'HungMongKai8.jpg',
     options: [
-      { id: 'HungMongKai8-1', text: '啊哈，正好，兩岸本一家，同意啊，以後還會多很多感恩戴德我這1%的99%選票。', points: 10, isCorrect: true },
-      { id: 'HungMongKai8-2', text: '越來越多中配在抖音宣傳要武統台灣，太可惡了，我不能提這種幫中國洗人口的提案。', points: 5, isCorrect: false },
-      { id: 'HungMongKai8-3', text: '其實中配待遇已經優於外籍配偶了，我持保留態度。', points: 3, isCorrect: false },
+      { id: 'HungMongKai8-1', text: '先冷靜協商啦，不然又要演全武行，對票倉傷很大。', points: 10, isCorrect: false },
+      { id: 'HungMongKai8-2', text: '找總召討論，大家帶枕頭去守夜兼打地鋪。', points: 5, isCorrect: false },
+      { id: 'HungMongKai8-3', text: '他們敢進來，我就燒立法院，看誰比較瘋。', points: 1969, isCorrect: true },
     ],
+    explanation: {
+      text: '2024/12/18多位民進黨立委摸黑闖入立法院站裡主席台要對《選罷法》、《憲訴法》與《財劃法》三讀作抗爭，立法院門口爆發推擠，國民黨立委鄭天財甚至口出狂言「你們全部都進去，我就把它燒掉」',
+      reference: 'https://www.ftvnews.com.tw/news/detail/2024C19W0444'
+    }
   },
   {
     id: 'HungMongKai9',
-    text: '總召說不管怎樣，你們給我把預算都刪掉30%',
+    text: '自稱金門坦克的玉珍姐說為了金門發財…發展，我們要修改離島條例18條。',
     image: 'HungMongKai9.jpg',
     options: [
-      { id: 'HungMongKai9-1', text: '阿，我看不懂那些，隨便弄個理由刪除好了', points: 12, isCorrect: true },
-      { id: 'HungMongKai9-2', text: '你腦子有洞，開班會也不是這樣舉手表決不討論阿。', points: 6, isCorrect: false },
-      { id: 'HungMongKai9-3', text: '不行，我要認真審預算', points: 4, isCorrect: false },
+      { id: 'HungMongKai9-1', text: '只要玉珍開口，票一定掉，絕對不要讓我連署這種案。', points: 12, isCorrect: false },
+      { id: 'HungMongKai9-2', text: '拒絕，這提案像極了一帶一路，包裝再好也不能偷渡統戰資源。', points: 6, isCorrect: false },
+      { id: 'HungMongKai9-3', text: '姊說什麼就是金門福音，我負責幫她舉手。', points: 1766, isCorrect: true },
     ],
+    explanation: {
+      text: '陳玉珍日前針對「離島建設條例」第18條之1的第6項提出修正草案，明定大陸地區人民、法人、團體、機構、資金、物品、商品、勞務及服務，進出離島自由貿易示範區，相關辦法由事業主管機關擬定；並於說明欄第7點述明，為便利「並適度鬆綁相關規定」。',
+      reference: 'https://www.cna.com.tw/news/aipl/202504110281.aspx'
+    }
   },
   {
     id: 'HungMongKai10',
-    text: '金門坦克，不是啦，玉珍姐說為了金門發財..發展，我們要修改離島條例18條。',
+    text: '這會期就快過了，中央政府總預算今天才要審，要熬夜表決完七百多份提案，是在哭喔⋯⋯',
     image: 'HungMongKai10.jpg',
     options: [
-      { id: 'HungMongKai10-1', text: '坦克...姊對金門最熟悉了，跟著他就對了。', points: 10, isCorrect: true },
-      { id: 'HungMongKai10-2', text: '只要是陳玉珍提的，一定就會吸引砲火讓我掉支持度，拒絕啦。', points: 5, isCorrect: false },
-      { id: 'HungMongKai10-3', text: '提案怪怪的ㄟ，開放大陸醫生、資源進入金門，這不就一帶一路的套路嗎?不行啦。', points: 3, isCorrect: false },
+      { id: 'HungMongKai10-1', text: '人不用醒，手能舉就好，投完再說。', points: 3838, isCorrect: true },
+      { id: 'HungMongKai10-2', text: '法案還改個沒完，怎麼讓我們正常表決？我要去抗議。', points: 5, isCorrect: false },
+      { id: 'HungMongKai10-3', text: '不怕刪預算，但連衛福部都刪到剩一塊錢，這不是亂搞是什麼？', points: 3, isCorrect: false },
     ],
-  },
-  {
-    id: 'HungMongKai11',
-    text: '你同事徐欣瑩用鎖住會議室大絕招，用三分鐘把超嚴格選罷法提案送出委員會，你怎麼看？',
-    image: 'HungMongKai11.jpg',
-    options: [
-      { id: 'HungMongKai11-1', text: '當然要鎖門自己審啊！懶得聽民進黨廢話，不愧是我的天才同事', points: 8, isCorrect: true },
-      { id: 'HungMongKai11-2', text: '超不爽，這麼高調的做法跟戒嚴沒兩樣，會害我回地方很難跟選民交代', points: 4, isCorrect: false },
-      { id: 'HungMongKai11-3', text: '不同聲音應經過充分討論，才是國會殿堂該有的民主精神', points: 2, isCorrect: false },
-    ],
-  },
-  {
-    id: 'HungMongKai12',
-    text: '這會期就快過了，中央政府總預算今天才要審，要熬夜表決完七百多份提案，是在哭喔⋯⋯',
-    image: 'HungMongKai12.jpg',
-    options: [
-      { id: 'HungMongKai12-1', text: '薪水小偷我最會，跟著總召投票準沒錯！', points: 20, isCorrect: true },
-      { id: 'HungMongKai12-2', text: '等等，好多都刪除中央的業務費，甚至有些部會刪到剩1元，政府還能運作嗎？', points: 10, isCorrect: false },
-      { id: 'HungMongKai12-3', text: '夭壽啊，怎麼有些法案要表決了提案還在改？我要去跟總召反應', points: 5, isCorrect: false },
-    ],
+    explanation: {
+      text: '國民黨及民眾黨合力提出案海攻勢，出現許多理由、金額和項目都令外界匪夷所思的提案。在民意壓力下整合至 700 多案進入表決大戰並在藍白聯手優勢人數下通過。期間更出現邊表決邊改提案、沒看到修正文字就表決等荒腔走板的議事失序。',
+      reference: 'https://watchout.tw/reports/tbHOpYA5buuRfEInq1Kh'
+    }
   },
 ];
 //羅明才 LoMingTsai
