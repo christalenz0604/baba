2a8627ae76bbf76847af6cfd01041d7eaa6eb43a
Christa <christalenz800@gmail.com>
2025-06-18 13:37:10 +0800

    charachter url to .gif

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -20,7 +20,7 @@ export const characters: Character[] = [
     id: 'linTeFu',//人物id
     name: '林德福',
     districts:'新北市第9選區',
-    avatar: getImagePath('/images/character/linTeFu_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/linTeFu_pic_Pickup.gif'),
     description: '在永和經歷過市議員市長到立法委員已超過30個年頭，讓永和人最有感的是在國會殿堂閉目養神',
     questionSetId: 'linTeFu',//對應問題系列
     score: 29474,
@@ -34,7 +34,7 @@ export const characters: Character[] = [
     id: 'HSUCHIAOHSIN',//人物id
     name: '徐巧芯',
     districts:'臺北市第7選區',
-    avatar: getImagePath('/images/character/HSUCHIAOHSIN_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/HSUCHIAOHSIN_pic_Pickup.gif'),
     description: '我有中指，我驕傲！',
     questionSetId: 'HSUCHIAOHSIN',//對應問題系列
     score: 41201,
@@ -49,7 +49,7 @@ export const characters: Character[] = [
     id: 'LaiShyhBao',//人物id
     name: '賴士葆',
     districts:'臺北市第8選區',
-    avatar: getImagePath('/images/character/LaiShyhBao_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/LaiShyhBao_pic_Pickup.gif'),
     description: '賴士葆曾高調批評「禮讓行人是白痴政策」。卻親身示範了這項政策存在的必要性——駕車撞上行人後，他將責任歸咎於天色昏暗、A柱死角，並暗指行人邊走邊聊天沒注意車輛。',
     questionSetId: 'LaiShyhBao',//對應問題系列
     score: 34040,
@@ -63,7 +63,7 @@ export const characters: Character[] = [
     id: 'wanghungwei',//人物id
     name: '王鴻薇',
     districts:'臺北市第3選區',
-    avatar: getImagePath('/images/character/wanghungwei_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/wanghungwei_pic_Pickup.gif'),
     description: '曾加入新黨、擔任台北市議員時是中國央視熱門來賓、不避諱表態親中立場，專長技能：肉搜並曝光罷免自己的公民個資。',
     questionSetId: 'wanghungwei',//對應問題系列
     score: 45818,
@@ -77,7 +77,7 @@ export const characters: Character[] = [
     id: 'LEEYEHSIU',//人物id
     name: '李彥秀',
     districts:'臺北市第4選區',
-    avatar: getImagePath('/images/character/LEEYEHSIU_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/LEEYEHSIU_pic_Pickup.gif'),
     description: '疑美是工作 移美是生活',
     questionSetId: 'LEEYEHSIU',//對應問題系列
     score: 45183,
@@ -91,7 +91,7 @@ export const characters: Character[] = [
   //   id: 'ChenYuJen',//人物id
   //   name: '陳玉珍',
   //   districts:'新北市第9選區',
-  //   avatar: '/images/陳玉珍_8bit.png?auto=compress&cs=tinysrgb&w=300',
+  //   avatar: '/images/陳玉珍_8bit.gif?auto=compress&cs=tinysrgb&w=300',
   //   description: '陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍陳玉珍',
   //   questionSetId: 'ChenYuJen',//對應問題系列
   // },
@@ -100,7 +100,7 @@ export const characters: Character[] = [
     id: 'YEHYUANCHIH',//人物id
     name: '葉元之',
     districts:'新北市第7選區',
-    avatar: getImagePath('/images/character/YEHYUANCHIH_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/YEHYUANCHIH_pic_Pickup.gif'),
     description: '想當我的助理嗎？你需要接受下列條件每日接受言語羞辱，不得生病請假，恩...還有什麼呢？算了，算了，我要去準備上通告了！',
     questionSetId: 'YEHYUANCHIH',//對應問題系列
     score: 32236,
@@ -114,7 +114,7 @@ export const characters: Character[] = [
   //   id: 'FUKUNCHI',//人物id
   //   name: '傅崐萁',
   //   districts:'花蓮縣',
-  //   avatar: '/images/傅崐萁8bit.png?auto=compress&cs=tinysrgb&w=300',
+  //   avatar: '/images/傅崐萁8bit.gif?auto=compress&cs=tinysrgb&w=300',
   //   description: '代表花蓮的立法委員，在0403大地震，太魯閣滿目瘡痍、搜救人員努力搜救之時，帶著國會立法委員前往敵對勢力國家接旨',
   //   questionSetId: 'FUKUNCHI',//對應問題系列
   // },
@@ -123,7 +123,7 @@ export const characters: Character[] = [
     id: 'HungMongKai',//人物id
     name: '洪孟楷',
     districts:'新北市第1選區',
-    avatar: getImagePath('/images/character/HungMongKai_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/HungMongKai_pic_Pickup.gif'),
     description: '媽寶立委;擅長跳針與甩鍋，專精無邏輯辯護術，出招自帶迷因效果',
     questionSetId: 'HungMongKai',//對應問題系列
     score: 54916,
@@ -137,7 +137,7 @@ export const characters: Character[] = [
     id: 'LoMingTsai',//人物id
     name: '羅明才',
     districts:'新北市第11選區',
-    avatar: getImagePath('/images/character/LoMingTsai_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/LoMingTsai_pic_Pickup.gif'),
     description: '2024年亦成為目前立法院黨團中最資深的「八連霸」立委，其父羅福助為天道盟創辦人，2012年獲罪潛逃大陸並被通緝至今。',
     questionSetId: 'LoMingTsai',//對應問題系列
     score: 34076,
@@ -151,7 +151,7 @@ export const characters: Character[] = [
     id: 'LIAOHsienHsiang',//人物id
     name: '廖先翔',
     districts:'新北市第12選區',
-    avatar: getImagePath('/images/character/LIAOHsienHsiang_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/LIAOHsienHsiang_pic_Pickup.gif'),
     description: '被稱為廖先蝦。因刪減外交部預算，用超蝦理由：「中國」未依約採購宏都拉斯白蝦。還主動提案刪除汐止當地社宅計畫。再加林林總總的疑似國土侵占疑雲，被認為不懂人間疾苦。',
     questionSetId: 'LIAOHsienHsiang',//對應問題系列
     score: 36399,
@@ -165,7 +165,7 @@ export const characters: Character[] = [
     id: 'LOCHIHCHIANG',//人物id
     name: '羅智強',
     districts:'臺北市第6選區',
-    avatar: getImagePath('/images/character/LOCHIHCHIANG_pic_Pickup.png'),
+    avatar: getImagePath('/images/character/LOCHIHCHIANG_pic_Pickup.gif'),
     description: '又名大安區閒人，擅長沾醬油，常運用各種話題為自己製造聲量。號稱自己日行萬步，跟著「皮克敏」散步到月球。',
     questionSetId: 'LOCHIHCHIANG',//對應問題系列
     score: 36398,
