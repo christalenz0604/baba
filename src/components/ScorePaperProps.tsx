fed0724fa58f58fa02d0d78e856a825ab24a0403
Christa <christalenz800@gmail.com>
2025-06-18 14:02:33 +0800

    staement css

--- src/components/ScorePaperProps.tsx
+++ src/components/ScorePaperProps.tsx
@@ -12,7 +12,7 @@ const ScoreTree: React.FC<ScorePaperProps> = ({ score, maxScore, level }) => {
   return (
     <div className="flex flex-row p-4">
       <div className="mb-2">
-        <div className="text-sm text-gray-100 font-pixel font-semibold">累積人民的怒氣值</div>
+        <div className="text-sm text-gray-100 font-pixel font-semibold">累積的連署書</div>
         <div className="flex items-center text-sm text-gray-100 font-pixel font-semibold">
           <img src={getImagePath(`/images/question/staement_lv${level}.png`)} alt="連署書數量" className="w-10 h-auto mr-2" />
           <span className="text-sm text-gray-100 font-pixel font-semibold">x {score}</span>
