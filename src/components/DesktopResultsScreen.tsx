20cb9996ae8eed53fb182d01dac88f801af7fa8f
Christa <christalenz800@gmail.com>
2025-06-21 14:13:24 +0800

    feat/fix: rwd voting date text aria with enough space

--- src/components/DesktopResultsScreen.tsx
+++ src/components/DesktopResultsScreen.tsx
@@ -218,7 +218,7 @@ const DesktopResultsScreen: React.FC = () => {
 
       <div className="flex-frow max-w-4xl mx-auto" >
         <motion.div
-          className="flex flex-col rounded-0 overflow-visible justify-center my-4 relative"
+          className="flex flex-col rounded-0 overflow-visible justify-center my-4 relative mx-auto lg:w-[90%]"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
